e0ae4ac71dcfe78c7fc3da2bfcd77cd6cfde3a93
Joey DeFrancesco <jdefr89@gmail.com>
2013-07-26 22:54:13 -0400

    added short.py and its unit test. the short.py module to be used to create the short links with base62 conversion.

---
+++ shorts/short.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+# (c) smalr.io
+# Author: Joey DeFrancesco
+# File: baseconv.py (short.py)
+
+import types
+
+# Base we will use for conversions Ex: 125_base10 = cb_base62
+BASE = 62
+
+# Offset of characters into ASCII table
+DIGIT_OFFSET = 48 # 48 is ASCII 0
+UPPERCASE_OFFSET = 55 # 65 - 10
+LOWERCASE_OFFSET = 61 # 97 - 36 (gives us our last characters - ends at 61st offset)
+
+# LAYOUT OF CHARACTER TABLE
+# [0-9A-Za-z] 
+
+# Define exceptions
+class ConvException(Exception): pass
+class OutOfRangeError(ConvException): pass
+class NonIntegerError(ConvException): pass
+class InvalidBase62Value(ConvException): pass
+
+
+# The following functions convert from base62 to Integer and vica-versa
+def base62_map_integer(char_value):
+    """
+    Take an character from base62 system and turn back into
+    integer equivalent...
+    Ex: a --> 36 (a maps to 36)
+    """
+
+    if char_value.isdigit():
+        return ord(char_value) - DIGIT_OFFSET 
+    elif 'A' <= char_value <= 'Z':
+        return ord(char_value) - UPPERCASE_OFFSET
+    elif 'a' <= char_value <= 'z':
+        return ord(char_value) - LOWERCASE_OFFSET
+    else:
+        raise ValueError("%s is not a valid character!" % char_value)
+
+    # -- EOFunc
+
+def integer_map_base62(int_value):
+    """
+    Take an integer and produce its base62 character equivalent
+    Ex: 36 --> a  (that is 36 maps to character a)
+    """
+
+    if int_value< 10:
+        return chr(int_value + DIGIT_OFFSET)
+    elif 10 <= int_value <= 35:
+        return chr(int_value + UPPERCASE_OFFSET)
+    elif 36 <= int_value < 62:
+        return chr(int_value + LOWERCASE_OFFSET)
+    else:
+        return ValueError("%d is not a valid integer [0-61]!", int_value)
+
+    # -- EOFunc
+
+def base10_to_base62(number):
+    """
+    Take a number and convert it to a list of base62 values
+    Ex: let f() be current function. f('125') = [2, 1]
+    """
+
+    # We shouldn't get here (sanity check)
+    if not isinstance(number, int):
+        raise NonIntegerError("Supplied parameter was not an integer")
+        number = int(number) # Give us an integer
+
+    # If we have a negative number, raise exception and covert to positive integer
+    if number < 0:
+        raise OutOfRangeError("Negative value supplied..")
+        number = abs(number)
+
+    digits = []
+
+    while number > 0:
+        remainder = number % BASE
+        digits.append(remainder)
+        number /= BASE 
+
+    digits.reverse() # We pushed values onto list backwards
+
+    return digits
+
+    # -- EOFunc
+
+def base62_to_base10(b62_value):
+    """
+    Convert value that is currently base-62 back to base-10 
+    equivalent.
+    """
+
+    if type(b62_value) is not types.StringType:
+        raise InvalidBase62Value("Parameter should be string")
+
+    # Convert digits to a list and then reverse them to calculate integer equiv.
+    digit_list = list(b62_value)
+    digit_list.reverse()
+   
+    # Convert base-62 to base-10: (xN*62^N)+(x1*62^1)+(x0*62^0)  
+    sum = 0
+    pow = 0
+    for digit in digit_list:
+        digit = base62_map_integer(digit)
+        sum += digit * (BASE**pow)
+        pow += 1
+
+    return sum
+
+def value_encode62(int_value):
+    """
+    Takes an integer value for input and returns the base62
+    equivalent
+    This is the interface that will be called when shortnening a link
+    """
+    # Be sure we are dealing with an integer
+    int_value = int(int_value)
+
+    # If negative convert to positive
+    if int_value < 0:
+        int_value = abs(int_value)
+
+    # value_list will contain a list of digits (base 62) that constitute the value
+    value_list = base10_to_base62(int_value)
+
+    # Create a string that equivalent to our input integer
+    encoded_string = ""
+    for value in value_list:
+        encoded_string += integer_map_base62(value)
+
+    # return our 'short' url postfix
+    return encoded_string
+
+    # -- EOFunc
+
+
+# LinkShortner Class - Creats and manipulates shortlinks..
+class LinkShortner(object):
+
+    def __init__(self):
+        pass
+
+
+def main():
+
+    pass
+
+    # -- EOFunc
+
+if __name__ == '__main__': pass 
---
+++ shorts/short_test.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+# By: Joey DeFrancesco 
+# (c) smalr.io
+# File: short_test.py
+# Testing module for short.py (to be renamed to baseconv.py) which is in charge of converting to and from base62. 
+# (see short.py docs)
+
+import unittest
+
+# Module we are testing
+import short
+
+# Test for success
+class Base10To62Known(unittest.TestCase):
+    known_values = ((1, '1'),
+                    (2, '2'),
+                    (10, 'A'),
+                    (36, 'a'),
+                    (124, '20')
+                   )
+    def setUp(self): pass
+
+    def tearDown(self): pass
+
+    def testTo62KnownValues(self):
+        """
+        Test that an integer for input produces the correct
+        base 62 equivalent (essentially our short url string)
+        """
+        print 'In testTo62KnownValues...'
+        for integer, val in self.known_values:
+            result = short.value_encode62(integer)
+            self.assertEqual(val, result)
+
+    
+class To62InvalidInput(unittest.TestCase):
+    
+    def setUp(self): pass
+
+    def tearDown(self): pass
+
+    def testNegative(self):
+        """base10_to_base62 should fail with no negative input"""
+        print 'In test for base10_to_base62 (testing negative input)'
+        self.assertRaises(short.OutOfRangeError, short.base10_to_base62, -1)
+      
+    def testNoneInteger(self):
+        """base10_to_base62 should fail, input must be integer (not decimal)"""
+        print 'In test for base10_to_base62. Testing non-integer values'
+        self.assertRaises(short.NonIntegerError, short.base10_to_base62, 0.5)
+        
+    
+class Base62To10Known(unittest.TestCase):
+    known_values = (('W', 32),
+                    ('a', 36),
+                    ('Qm', 1660),
+                    ('fq', 2594),
+                    ('mJ', 2995),
+                    ('Zo', 2220),
+                    ('1J', 81),
+                    ('jM', 2812),
+                    ('182', 4342),
+                    ('CT0vN28C', 43907429847324))
+
+    def setUp(self): pass
+
+    def tearDown(self): pass
+
+    def testFrom62KnownValues(self): 
+        print 'In testFrom62KnownValues'
+        for seq, integer in self.known_values:
+            result = short.base62_to_base10(seq)
+            self.assertEqual(result, integer)
+
+    
+class From62InvalidInput(unittest.TestCase):
+
+    def setUp(self): pass
+
+    def tearDown(self): pass
+
+    def testNegative(self):
+        pass
+
+    def testNoneStringParamer(self):
+        pass
+
+    
+if __name__ == '__main__':
+    unittest.main()
