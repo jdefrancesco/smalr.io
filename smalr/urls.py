5694501c841c21d378dc36f7bb4004a6bd2bbe90
Madhax <chinchilla@gmail.com>
2013-09-23 15:11:05 -0400

    added interface for ajax calls updated views to reply via JSON added interface outlining future functionality

--- smalr/urls.py
+++ smalr/urls.py
@@ -16,8 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^shorts/', include('shorts.urls')),
-    url(r'^shorten/$', shorten),
+    url(r'^0/shorten/$', shorten),
+    url(r'^0/status/$', status),
     url(r'^(?P<key>\w+)$', redirect),
     url(r'', index),
 )
--- smalr/views.py
+++ smalr/views.py
@@ -1,17 +1,18 @@
 ##############################################################################################
 # Author: in70x, Madhax
-# Date:   8/21/2013
+# Date:   9/21/2013
 # 
 # Description: smalr.io views file
+# Major TODO: add url dispatcher for custom length URLs
 #
 #
 #
-#
-#
+# Last Updated: 9/23/2013
 # Copyright (c) smalr.io
 ################################################################################################
 
 import datetime
+import json
 from string import join
 
 from django.contrib.auth.decorators import login_required
@@ -23,17 +24,16 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ObjectDoesNotExist
-
-
+from django.core import serializers
 
 from smalr.models import *
 from shorts.short import *
 
 def shorten(request):
-    #@TODO Finish Shorten URL
     p = request.POST
     if p["url"]:
         url = p["url"]
+        #@TODO fix custom URL collission
         try: #get next 'dynamic' url
             url_key = state.objects.get(pk=1)
             url_key.urls_head += 1
@@ -45,11 +45,34 @@ def shorten(request):
             state.objects.create(urls_head=1)
         #insert short URL
         urls.objects.create(key=url_key, status=0, hit_count=0, safety_rating=0.0, url=url)
-        plaintext_url = base10_to_base62(url_key)      
+        plaintext_url = value_encode62(url_key)  
         #@TODO get a list of all recent URLs created by this user (via acct_id or session)
-    #return HttpResponseRedirect("http://smalr.io/")
-    return render_to_response("fake-landing.html")
+    output = []
+    now = datetime.datetime.now()
+    output.append([plaintext_url, url, 0, 0.0, now.strftime("%Y-%m-%d %H:%M")])
+    data = json.dumps(output)
+    return HttpResponse(data, mimetype='application/json')
+
+def custom_shorten(request):
+    #@TODO Finish custom URLS
+    p = request.POST
+    if p["url"]:
+        pass
+    return("")
+
+
+#check if custom URL is available, will return  true/false via ajax on keyboard input
+def is_valid_custom(request):
+    #@TODO Finish is valid
+    p = request.POST
+    if p["url"]:
+        pass
+    return("")
     
+    
+def status():
+    #@TODO Finish status pages
+    return("")
     
 
 def redirect(request, key):
@@ -66,12 +89,13 @@ def redirect(request, key):
     return HttpResponseRedirect("")
 
 def main(request):
-    return render_to_response("fake-index.html")
+    return render_to_response("index.html")
 
 def index(request):
+    #@TODO find better placement for template
     c = {}
     c.update(csrf(request))
-    return render_to_response("fake-index.html", c)
+    return render_to_response("index.html", c)
 
 def register(request):
     return HttpResponseRedirect("")
